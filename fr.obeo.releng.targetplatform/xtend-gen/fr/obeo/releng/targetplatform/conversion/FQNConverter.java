80e5c9371d995b8714eb639c1a3001f0ed18facb
Mikaël Barbero <mikael.barbero@gmail.com>
2014-03-03 15:31:40 +0100

    This xtend generated class was ignored and was failing the build

---
+++ fr.obeo.releng.targetplatform/xtend-gen/fr/obeo/releng/targetplatform/conversion/FQNConverter.java
@@ -0,0 +1,47 @@
+package fr.obeo.releng.targetplatform.conversion;
+
+import com.google.common.collect.ImmutableSet;
+import java.util.Set;
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.conversion.impl.AbstractNullSafeConverter;
+import org.eclipse.xtext.nodemodel.INode;
+
+/**
+ * Copy/paste from Mwe2ValueConverters
+ */
+@SuppressWarnings("all")
+public class FQNConverter extends AbstractNullSafeConverter<String> {
+  private Set<String> allKeywords;
+  
+  public FQNConverter(final Grammar grammar) {
+    Set<String> _allKeywords = GrammarUtil.getAllKeywords(grammar);
+    ImmutableSet<String> _copyOf = ImmutableSet.<String>copyOf(_allKeywords);
+    this.allKeywords = _copyOf;
+  }
+  
+  public String internalToValue(final String string, final INode node) {
+    return string.replaceAll("[\\^\\s]", "");
+  }
+  
+  public String internalToString(final String value) {
+    final String[] segments = value.split("\\.");
+    int _length = value.length();
+    final StringBuilder builder = new StringBuilder(_length);
+    boolean first = true;
+    for (final String segment : segments) {
+      {
+        if ((!first)) {
+          builder.append(".");
+        }
+        boolean _contains = this.allKeywords.contains(segment);
+        if (_contains) {
+          builder.append("^");
+        }
+        builder.append(segment);
+        first = false;
+      }
+    }
+    return builder.toString();
+  }
+}
