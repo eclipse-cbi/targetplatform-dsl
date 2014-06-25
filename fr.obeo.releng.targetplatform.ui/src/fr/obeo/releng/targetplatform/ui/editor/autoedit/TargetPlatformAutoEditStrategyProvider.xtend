d96c1397308487be2fd7c0d3edbb92453e8a2dc4
Mikaël Barbero <mikael.barbero@gmail.com>
2014-06-25 14:11:22 +0200

    Default auto edit behavior for curly braces blocks only

--- fr.obeo.releng.targetplatform.ui/src/fr/obeo/releng/targetplatform/ui/editor/autoedit/TargetPlatformAutoEditStrategyProvider.xtend
+++ fr.obeo.releng.targetplatform.ui/src/fr/obeo/releng/targetplatform/ui/editor/autoedit/TargetPlatformAutoEditStrategyProvider.xtend
@@ -2,11 +2,18 @@ package fr.obeo.releng.targetplatform.ui.editor.autoedit
 
 import org.eclipse.xtext.ui.editor.autoedit.DefaultAutoEditStrategyProvider
 import org.eclipse.xtext.ui.editor.autoedit.AbstractEditStrategyProvider.IEditStrategyAcceptor
+import org.eclipse.jface.text.IDocument
 
 class TargetPlatformAutoEditStrategyProvider extends DefaultAutoEditStrategyProvider {
 	
 	override protected configureCompoundBracesBlocks(IEditStrategyAcceptor acceptor) {
-		
+		acceptor.accept(compoundMultiLineTerminals.newInstanceFor("{", "}"), IDocument.DEFAULT_CONTENT_TYPE);
+	}
+	
+	override protected configureSquareBrackets(IEditStrategyAcceptor acceptor) {
+	}
+	
+	override protected configureParenthesis(IEditStrategyAcceptor acceptor) {
 	}
 	
 }
--- fr.obeo.releng.targetplatform.ui/xtend-gen/fr/obeo/releng/targetplatform/ui/editor/autoedit/TargetPlatformAutoEditStrategyProvider.java
+++ fr.obeo.releng.targetplatform.ui/xtend-gen/fr/obeo/releng/targetplatform/ui/editor/autoedit/TargetPlatformAutoEditStrategyProvider.java
@@ -1,10 +1,20 @@
 package fr.obeo.releng.targetplatform.ui.editor.autoedit;
 
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.xtext.ui.editor.autoedit.AbstractEditStrategyProvider;
+import org.eclipse.xtext.ui.editor.autoedit.CompoundMultiLineTerminalsEditStrategy;
 import org.eclipse.xtext.ui.editor.autoedit.DefaultAutoEditStrategyProvider;
 
 @SuppressWarnings("all")
 public class TargetPlatformAutoEditStrategyProvider extends DefaultAutoEditStrategyProvider {
   protected void configureCompoundBracesBlocks(final AbstractEditStrategyProvider.IEditStrategyAcceptor acceptor) {
+    CompoundMultiLineTerminalsEditStrategy _newInstanceFor = this.compoundMultiLineTerminals.newInstanceFor("{", "}");
+    acceptor.accept(_newInstanceFor, IDocument.DEFAULT_CONTENT_TYPE);
+  }
+  
+  protected void configureSquareBrackets(final AbstractEditStrategyProvider.IEditStrategyAcceptor acceptor) {
+  }
+  
+  protected void configureParenthesis(final AbstractEditStrategyProvider.IEditStrategyAcceptor acceptor) {
   }
 }
