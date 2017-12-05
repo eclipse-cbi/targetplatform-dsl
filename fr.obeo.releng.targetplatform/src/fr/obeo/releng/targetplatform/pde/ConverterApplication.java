a95f5b0a6486be3f2acf3cfc38422dd660d3afc8
Philippe DUL <philippe.dul@thalesgroup.com>
2017-12-05 13:40:43 +0100

    Add some logs
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- fr.obeo.releng.targetplatform/src/fr/obeo/releng/targetplatform/pde/ConverterApplication.java
+++ fr.obeo.releng.targetplatform/src/fr/obeo/releng/targetplatform/pde/ConverterApplication.java
@@ -26,131 +26,130 @@ import com.google.inject.Injector;
 import fr.obeo.releng.targetplatform.TargetPlatformBundleActivator;
 import fr.obeo.releng.targetplatform.TargetPlatformStandaloneSetup;
 
-
 /**
  * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
  *
  */
 public class ConverterApplication implements IApplication {
 
-	/** 
-	 * {@inheritDoc}
-	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
-	 */
-	@Override
-	public Object start(IApplicationContext context) throws Exception {
-		Map<?,?> arguments = context.getArguments();
-		String[] args = (String[]) arguments.get(IApplicationContext.APPLICATION_ARGS);
-		String path;
-		if (args.length <= 0) {
-			System.out.println("Must provide path to a target form file");
-			return -256;
-		} else {
-			path = args[0];
-		}
-		
-		TargetPlatformStandaloneSetup.doSetup();
-		Injector injector = TargetPlatformBundleActivator.getInstance().getInjector(TargetPlatformBundleActivator.TARGET_PLATFORM_LANGUAGE_NAME);
-		Converter converter = new Converter();
-		injector.injectMembers(converter);
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
+   */
+  @Override
+  public Object start(IApplicationContext context) throws Exception {
+    Map<?, ?> arguments = context.getArguments();
+    String[] args = (String[]) arguments.get(IApplicationContext.APPLICATION_ARGS);
+    String path;
+    if (args.length <= 0) {
+      System.out.println("Must provide path to a target form file");
+      return -256;
+    } else {
+      path = args[0];
+    }
 
-		URI uri = normalize(org.eclipse.emf.common.util.URI.createURI(path));
-		
-		Diagnostic diagnostic = converter.generateTargetDefinitionFile(uri, createPrintingMonitor());
-		
-		if (diagnostic.getSeverity() >= Diagnostic.WARNING) {
-			for (Diagnostic child : diagnostic.getChildren()) {
-				printDiagnostic(child, "");
-			}
-		}
-		
-		if (diagnostic.getSeverity() == Diagnostic.ERROR) {
-			System.out.println("Problems occurred during generation of target platform definition file.");
-			return -1;
-		} else if (diagnostic.getSeverity() == Diagnostic.CANCEL) {
-			System.out.println("Operation cancelled.");
-			return -2;
-		} else {
-			System.out.println("The target platform definition file has been successfully generated.");
-			return 0;
-		}
-	}
+    TargetPlatformStandaloneSetup.doSetup();
+    Injector injector = TargetPlatformBundleActivator.getInstance()
+        .getInjector(TargetPlatformBundleActivator.TARGET_PLATFORM_LANGUAGE_NAME);
+    Converter converter = new Converter();
+    injector.injectMembers(converter);
 
-	private static IProgressMonitor createPrintingMonitor() {
-		return BasicMonitor.toIProgressMonitor(new BasicMonitor.Printing(System.out));
-	}
-	
-	private static void printDiagnostic(Diagnostic diagnostic, String indent) {
-		if (diagnostic.getSeverity() > Diagnostic.OK) {
-			System.out.print(indent);
-			final String severity = getSeverityString(diagnostic);
-			System.out.println(severity + " " + diagnostic.getMessage());
-			for (Iterator<Diagnostic> i = diagnostic.getChildren().iterator(); i.hasNext();) {
-				printDiagnostic((Diagnostic) i.next(), indent + "  ");
-			}
-		}
-	}
+    URI uri = normalize(org.eclipse.emf.common.util.URI.createURI(path));
+    
+    Diagnostic diagnostic = converter.generateTargetDefinitionFile(uri, createPrintingMonitor());
 
-	private static String getSeverityString(Diagnostic diagnostic) {
-		final String severity;
-		switch (diagnostic.getSeverity()) {
-			case Diagnostic.OK:
-				severity = "[OK]     ";
-				break;
-			case Diagnostic.INFO:
-				severity = "[INFO]   ";
-				break;
-			case Diagnostic.WARNING:
-				severity = "[WARNING]";
-				break;
-			case Diagnostic.ERROR:
-				severity = "[ERROR]  ";
-				break;
-			case Diagnostic.CANCEL:
-				severity = "[CANCEL] ";
-				break;
-			default:
-				severity = Integer.toHexString(diagnostic.getSeverity());
-				break;
-		}
-		return severity;
-	}
-	
-	private static URI normalize(URI uri) {
-		String fragment = uri.fragment();
-		String query = uri.query();
-		URI trimmedURI = uri.trimFragment().trimQuery();
-		URI result = trimmedURI;
-		String scheme = result.scheme();
-		if (scheme == null) {
-			if (result.hasAbsolutePath()) {
-				result = URI.createURI("file:" + result);
-			} else {
-				result = URI.createFileURI(new File(result.toString())
-						.getAbsolutePath());
-			}
-		}
+    if (diagnostic.getSeverity() >= Diagnostic.WARNING) {
+      for (Diagnostic child : diagnostic.getChildren()) {
+        printDiagnostic(child, "");
+      }
+    }
 
-		if (result == trimmedURI) {
-			return uri;
-		}
+    if (diagnostic.getSeverity() == Diagnostic.ERROR) {
+      System.out.println("Problems occurred during generation of target platform definition file.");
+      return -1;
+    } else if (diagnostic.getSeverity() == Diagnostic.CANCEL) {
+      System.out.println("Operation cancelled.");
+      return -2;
+    } else {
+      System.out.println("The target platform definition file has been successfully generated.");
+      System.out.println(uri.toFileString());
+      return 0;
+    }
+  }
 
-		if (query != null) {
-			result = result.appendQuery(query);
-		}
-		if (fragment != null) {
-			result = result.appendFragment(fragment);
-		}
+  private static IProgressMonitor createPrintingMonitor() {
+    return BasicMonitor.toIProgressMonitor(new BasicMonitor.Printing(System.out));
+  }
 
-		return result;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.equinox.app.IApplication#stop()
-	 */
-	@Override
-	public void stop() {
-	}
+  private static void printDiagnostic(Diagnostic diagnostic, String indent) {
+    System.out.print(indent);
+    final String severity = getSeverityString(diagnostic);
+    System.out.println(severity + " " + diagnostic.getMessage());
+    for (Iterator<Diagnostic> i = diagnostic.getChildren().iterator(); i.hasNext();) {
+      printDiagnostic((Diagnostic) i.next(), indent + "  ");
+    }
+  }
+
+  private static String getSeverityString(Diagnostic diagnostic) {
+    final String severity;
+    switch (diagnostic.getSeverity()) {
+    case Diagnostic.OK:
+      severity = "[OK]     ";
+      break;
+    case Diagnostic.INFO:
+      severity = "[INFO]   ";
+      break;
+    case Diagnostic.WARNING:
+      severity = "[WARNING]";
+      break;
+    case Diagnostic.ERROR:
+      severity = "[ERROR]  ";
+      break;
+    case Diagnostic.CANCEL:
+      severity = "[CANCEL] ";
+      break;
+    default:
+      severity = Integer.toHexString(diagnostic.getSeverity());
+      break;
+    }
+    return severity;
+  }
+
+  private static URI normalize(URI uri) {
+    String fragment = uri.fragment();
+    String query = uri.query();
+    URI trimmedURI = uri.trimFragment().trimQuery();
+    URI result = trimmedURI;
+    String scheme = result.scheme();
+    if (scheme == null) {
+      if (result.hasAbsolutePath()) {
+        result = URI.createURI("file:" + result);
+      } else {
+        result = URI.createFileURI(new File(result.toString()).getAbsolutePath());
+      }
+    }
+
+    if (result == trimmedURI) {
+      return uri;
+    }
+
+    if (query != null) {
+      result = result.appendQuery(query);
+    }
+    if (fragment != null) {
+      result = result.appendFragment(fragment);
+    }
+
+    return result;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.eclipse.equinox.app.IApplication#stop()
+   */
+  @Override
+  public void stop() {
+  }
 }
